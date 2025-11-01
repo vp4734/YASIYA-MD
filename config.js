cecfafb9b9112b7c8ef05e1cea1de37e9f77cbd8
vp4734 <vpmd304@gmail.com>
2025-11-01 13:28:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~8IZHmTKL#cUQGmPzTpwpYaX2Q412CvjPnpmV9Sa9g49uEwuZTEtw'
 };
